e5341906f5b7c9d5a81fe0f711b033a407e2bb77
Changhoon-Han <hch4102@naver.com>
2020-12-08 16:40:29 +0900

    회원가입 버그픽스

--- HKLMart/src/main/java/com/hklmart/controller/MemberController.java
+++ HKLMart/src/main/java/com/hklmart/controller/MemberController.java
@@ -1,7 +1,5 @@
 package com.hklmart.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,12 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
