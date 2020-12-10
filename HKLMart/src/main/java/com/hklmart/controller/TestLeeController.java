09772eeee3d329cd777b730931966c4e01b138b8
admin <admin@DESKTOP-4T2TJ66>
2020-12-10 17:55:25 +0900

    상품 상세조회 페이지 커밋

---
+++ HKLMart/src/main/java/com/hklmart/controller/TestLeeController.java
@@ -0,0 +1,20 @@
+package com.hklmart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+@RequestMapping("/testLee/*")
+public class TestLeeController {
+	
+	
+	@GetMapping("/product-detail")
+	public String goProductDetail() {
+		return "/product-detail";
+	}
+	
+}
